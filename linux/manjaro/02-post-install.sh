d3f5571589a47792ac1f5034e255f219bad4f76a
Flavio Sousa <flaviosousa@cvccorp.com.br>
2023-02-10 17:54:34 -0300

    Fix oh-my-zsh auto confirm installation

--- linux/manjaro/02-post-install.sh
+++ linux/manjaro/02-post-install.sh
@@ -36,7 +36,7 @@ done < "$input"
 echo -e "\n\n >>> Install Oh My ZSH \n"
 CHSH='yes'
 RUNZSH='no'
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
