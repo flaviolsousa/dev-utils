2675f8df7f211354b69fb7b0a1fe994f9b172703
Flavio Sousa <flaviosousa@cvc.com.br>
2023-02-18 01:24:44 -0300

    change snap package position

--- linux/manjaro/01-post-install.sh
+++ linux/manjaro/01-post-install.sh
@@ -48,6 +48,18 @@ pacman -Sy snapd pamac-snap-plugin --noconfirm
 # systemctl enable --now snapd.apparmor
 systemctl enable --now snapd.socket
 
+p_h2 "Install Snap Packages"
+input="./snap-packages.conf"
+while IFS= read -r line
+do
+  if [[ "$line" =~ ^[^#].* ]]; then
+    if [[ ! -z $line ]]; then
+      p_h2 "Install $line"
+      snap install $line
+    fi
+  fi
+done < "$input"
+
 p_h2 "Install Packages"
 pacman -Syu whois gnome-disk-utility --noconfirm
 
@@ -60,18 +72,6 @@ sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /home/$u/.bashrc
 
 p_h2 "Update packages"
 yay -Syu
-
-p_h2 "Install Snap Packages"
-input="./snap-packages.conf"
-while IFS= read -r line
-do
-  if [[ "$line" =~ ^[^#].* ]]; then
-    if [[ ! -z $line ]]; then
-      p_h2 "Install $line"
-      snap install $line
-    fi
-  fi
-done < "$input"
 
 p_h2 "Changing Shell to zsh"
 chsh -s $(which zsh) $u
